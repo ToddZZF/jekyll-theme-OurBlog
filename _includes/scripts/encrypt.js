9d6558e0a2efd9ec3dc041b7db341bde677ca4b0
ToddZZF <toddzzf@outlook.com>
2022-05-05 10:20:03 +0800

    add timeline and photos

--- _includes/scripts/encrypt.js
+++ _includes/scripts/encrypt.js
@@ -1,13 +1,42 @@
-(function(){
-    if('{{ page.password }}'){
-        if (prompt('猜猜密码是什么呀') !== '{{ page.password }}'){
-            alert('猜错了！');
-if (history.length === 1) {
-    window.opener = null;
-    window.open('', '_self');
-    window.close();
-            } else {
-                history.back();
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+(function () {
+    if ('{{ site.password }}') {
+        let password = getCookie("password");
+        if (password !== '{{ site.password }}') {
+            if (prompt('猜猜密码是什么呀') !== '{{ site.password }}') {
+                alert('猜错了！');
+                if (history.length === 1) {
+                    window.opener = null;
+                    window.open('', '_self');
+                    window.close();
+                } else {
+                    history.back();
+                }
+            }
+            else {
+                setCookie("password", '{{ site.password }}', 1);
             }
         }
     }
---
+++ _photos/stamps.md
@@ -0,0 +1,8 @@
+---
+title: stamps
+img_folder: /assets/images/letter-img/
+img_format: .png
+thumb_folder: /assets/thumbs/letter-img/
+thumb_format: .webp
+---
+
